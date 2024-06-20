b2799fa8262579cf5478f9f20e5ee00ac6adccce
Nikunj Bhanderi <113762162+BhanderiNikunj@users.noreply.github.com>
2024-06-20 21:49:24 +0530

    basic setup added

--- lib/common/global.dart
+++ lib/common/global.dart
@@ -1,6 +1,6 @@
 import 'package:get/get.dart';
 import 'package:unk/common/route_list.dart';
-import 'package:unk/screens/splash_screem/view/splash_screen.dart';
+import 'package:unk/screens/splash/view/splash_screen.dart';
 
 List<GetPage> getPages = [
   GetPage(name: RouteList.initial, page: () => const SplashScreen()),

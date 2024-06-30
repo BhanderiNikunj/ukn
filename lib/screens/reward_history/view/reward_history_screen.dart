07469a372d684432d0d67a34e6f5e246d9d61613
Nikunj Bhanderi <113762162+BhanderiNikunj@users.noreply.github.com>
2024-06-30 10:53:09 +0530

    ui changes done
    ui changes done

--- lib/screens/reward_history/view/reward_history_screen.dart
+++ lib/screens/reward_history/view/reward_history_screen.dart
@@ -24,8 +24,8 @@ class _RewardHistoryScreenState extends State<RewardHistoryScreen> {
     super.initState();
   }
 
-  @override 
-  void dispose(){
+  @override
+  void dispose() {
     controller.dispose();
     super.dispose();
   }
@@ -36,6 +36,7 @@ class _RewardHistoryScreenState extends State<RewardHistoryScreen> {
       title: Strings.reward_history,
       child: ListView.separated(
         itemCount: 50,
+        physics: const BouncingScrollPhysics(),
         separatorBuilder: (context, index) {
           if ((index + 1) % 5 == 0) {
             return Container(
@@ -60,25 +61,47 @@ class _RewardHistoryScreenState extends State<RewardHistoryScreen> {
           return CommonWidget.sizedBox(isShrink: true);
         },
         itemBuilder: (context, index) {
-          if (index % 4 == 0) {
+          if ((index + 1) % 5 == 0) {
             return CommonWidget.sizedBox(isShrink: true);
           }
           return Container(
-            height: 100,
             margin: EdgeInsets.symmetric(
               horizontal: 16.w,
               vertical: 10.h,
             ),
-            color: AppColor.primary1Color,
+            padding: EdgeInsets.symmetric(
+              horizontal: 8.w,
+              vertical: 9.h,
+            ),
+            decoration: BoxDecoration(
+              color: AppColor.white1Color,
+              borderRadius: BorderRadius.circular(10.r),
+              boxShadow: [
+                BoxShadow(
+                  blurRadius: 5,
+                  spreadRadius: 0,
+                  color: AppColor.primary6Color,
+                  offset: Offset(0.w, 0.h),
+                ),
+              ],
+            ),
             child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 CommonWidget.imageBuilder(
                   imagePath: Images.play_store_png,
                 ),
+                CommonWidget.sizedBox(width: 10),
                 Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     CommonWidget.commonText(
                       text: 'Google Play Store',
+                      color: AppColor.primary1Color,
+                    ),
+                    CommonWidget.commonText(
+                      text: CommonWidget.stringToRupee(text: '50'),
+                      color: AppColor.primary1Color,
                     ),
                   ],
                 ),

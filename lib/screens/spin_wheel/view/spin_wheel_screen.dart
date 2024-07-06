df5b54b673e1aadd42e42ffb7d7cf6604bcf8abd
UTKARSH KORAT <koratutkarsh310@gmail.com>
2024-07-06 19:29:28 +0530

    update_spin_wheel_screen

--- lib/screens/spin_wheel/view/spin_wheel_screen.dart
+++ lib/screens/spin_wheel/view/spin_wheel_screen.dart
@@ -1,7 +1,10 @@
 import 'dart:async';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_fortune_wheel/flutter_fortune_wheel.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:unk/common/colors.dart';
 import 'package:unk/common/common_widget.dart';
 
 class SpinWheelScreen extends StatefulWidget {
@@ -16,7 +19,6 @@ class _SpinWheelScreenState extends State<SpinWheelScreen> {
 
   @override
   void dispose() {
-    // TODO: implement dispose
     controller.close();
     super.dispose();
   }
@@ -25,30 +27,108 @@ class _SpinWheelScreenState extends State<SpinWheelScreen> {
   Widget build(BuildContext context) {
     return CommonWidget.commonScreenUI(
       title: "Spin",
-      child: Column(
-        children: [
-          Expanded(
-            child: FortuneWheel(
-              duration: Duration(seconds: 5),
-              selected: controller.stream,
-              items: [
-                FortuneItem(
-                  child: Text('Han Solo'),
-                ),
-                FortuneItem(
-                  child: Text('10000'),
-                ),
-                FortuneItem(
-                  child: Text('500'),
-                ),FortuneItem(
-                  child: Text('100'),
-                ),FortuneItem(
-                  child: Text('50'),
-                ),
-              ],
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          children: [
+            Expanded(
+              child: FortuneWheel(
+                animateFirst: false,
+                hapticImpact: HapticImpact.medium,
+                indicators: [
+                  FortuneIndicator(
+                    child: Transform.rotate(
+                      angle: 361.3,
+                      alignment: Alignment.topCenter,
+                      child: TriangleIndicator(
+                        elevation: 10,
+                        color: Color(0xffFFCA4F),
+                      ),
+                    ),
+                  ),
+                  FortuneIndicator(
+                    child: Container(
+                      width: 50.w,
+                      height: 50.h,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: AppColor.primary1Color,
+                        border: Border.all(
+                          width: 2,
+                          color: Color(0xffFFCA4F),
+                        ),
+                      ),
+                      child: Text(
+                        "SPIN",
+                        style: TextStyle(
+                          color: AppColor.white1Color,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+                selected: controller.stream,
+                items: [
+                  FortuneItem(
+                    child: Text('Han Solo'),
+                  ),
+                  FortuneItem(
+                    child: Text('10000'),
+                  ),
+                  FortuneItem(
+                    child: Text('500'),
+                  ),
+                  fortuneItemView(
+                      title: "9000",
+                      color: AppColor.secondery1Color,
+                      borderColor: AppColor.primary1Color)
+                  // FortuneItem(
+                  //   child: Text('100'),
+                  // ),
+                  // FortuneItem(
+                  //   child: Text('50'),
+                  // ),
+                  // FortuneItem(
+                  //   child: Text('50'),
+                  // ),
+                  // FortuneItem(
+                  //   child: Text('50'),
+                  // ),
+                  // FortuneItem(
+                  //   child: Text('50'),
+                  // ),
+                  // FortuneItem(
+                  //   child: Text('50'),
+                  // ),
+                  // FortuneItem(
+                  //   child: Text('50'),
+                  // ),
+                  // FortuneItem(
+                  //   child: Text('50'),
+                  // ),
+                  // FortuneItem(
+                  //   child: Text('50'),
+                  // ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
+      ),
+    );
+  }
+
+  FortuneItem fortuneItemView(
+      {required title, required Color color, required Color borderColor}) {
+    return FortuneItem(
+      child: Text(
+        title,
+      ),
+      style: FortuneItemStyle(
+        color: color,
+        borderColor: borderColor,
+        borderWidth: 2,
       ),
     );
   }

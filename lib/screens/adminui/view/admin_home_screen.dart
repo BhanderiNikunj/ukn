6f822fcb2b9074aecb1234438fd30528ccd0b155
Taili kamlesh <119474574+kunalsahu7@users.noreply.github.com>
2024-07-11 22:08:12 +0530

    admin-daseboredchange
    admin-daseboredchange

--- lib/screens/adminui/view/admin_home_screen.dart
+++ lib/screens/adminui/view/admin_home_screen.dart
@@ -12,14 +12,13 @@ class _AdminHomeScrrenState extends State<AdminHomeScrren> {
   Widget build(BuildContext context) {
     return CommonWidget.commonScreenUI(
       height: ScreenUtil().screenHeight * 0.85,
-      titleWidget:  Row(
+      titleWidget: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           CommonWidget.commonText(
-            text: "Admin HomeScreen",
-            fontWeight: FontWeight.bold,
-            color: Colors.white
-            ),
+              text: "Admin HomeScreen",
+              fontWeight: FontWeight.bold,
+              color: Colors.white),
         ],
       ),
       child: Stack(
@@ -29,54 +28,142 @@ class _AdminHomeScrrenState extends State<AdminHomeScrren> {
               borderRadius: const BorderRadius.only(
                 topLeft: Radius.circular(16),
                 topRight: Radius.circular(16),
-              )
+              ),
             ),
-           // height: double.infinity,
+            // height: double.infinity,
             child: Image.network(
-              'https://t4.ftcdn.net/jpg/04/93/66/85/360_F_493668569_jnWjXervcMN9X9Ucib2N2B4lbZspeAFX.jpg',
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDMDmGxYRWz_lysBfA9HZXa1NUKONcnsPzSAF110FEuhSRAB9IVjBrRiB-xIoO1CTq3fo&usqp=CAU',
               fit: BoxFit.cover,
               height: double.infinity,
-            ),),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 40,top: 12,left: 20,right: 20),
-              child: Align(
-                alignment: Alignment.bottomLeft,
-                child: Row(
-                  children: [
-                    InkWell(
-                      onTap: () => CommonRoute.pushNamed(
-                    page: RouteList.admin_user_data_screen,
-                  ),
-                      child: Container(
-                        padding: const EdgeInsets.all(12),
-                        decoration: BoxDecoration(
-                          color: Colors.orange,
-                          borderRadius: BorderRadius.circular(12),
-                          border: Border.all(
-                            color: Colors.black,
-                          )
+              
+            ),
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.only(bottom: 40, top: 12, left: 20, right: 20),
+            child: Align(
+              alignment: Alignment.bottomLeft,
+              child: Column(
+                children: [
+                   Row(
+                    children: [
+                      InkWell(
+                        onTap: () => CommonRoute.pushNamed(
+                          page: RouteList.admin_user_data_screen,
                         ),
-                        child: CommonWidget.commonText(text: "User Data"),
+                        child: Container(
+                          height: 100.h,
+                          width: 120.w,
+                          alignment: Alignment.center,
+                          padding: const EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            color: AppColor.primary1Color,
+                            borderRadius: BorderRadius.circular(12),
+                            border: Border.all(
+                              color: Colors.white,
+                            ),
+                          ),
+                          child: CommonWidget.commonText(text: "User Data",color: Colors.white),
+                        ),
                       ),
-                    ),
-                    const Spacer(),
-                    Container(
-                      padding: const EdgeInsets.all(12),
-                      decoration: BoxDecoration(
-                        color: Colors.orange,
-                        borderRadius: BorderRadius.circular(12),
-                        border: Border.all(
-                          color: Colors.black,
-                        )
+                      const Spacer(),
+                      Container(
+                        height: 100.h,
+                        width: 120.w,
+                        alignment: Alignment.center,
+                        padding: const EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            color: AppColor.primary1Color,
+                            borderRadius: BorderRadius.circular(12),
+                            border: Border.all(
+                              color: Colors.white,
+                            )),
+                        child: CommonWidget.commonText(text: "Chet Box",color: Colors.white),
                       ),
-                      child: CommonWidget.commonText(text: "Chet Box"),
-                    ),
-                  ],
-                ),
+                    ],
+                  ),
+                   const Spacer(),
+                   Row(
+                    children: [
+                      InkWell(
+                        onTap: () => CommonRoute.pushNamed(
+                          page: RouteList.admin_user_data_screen,
+                        ),
+                        child: Container(
+                        height: 100,
+                        width: 120,
+                          alignment: Alignment.center,
+                          padding: const EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            color:  AppColor.primary1Color,
+                            borderRadius: BorderRadius.circular(12),
+                            border: Border.all(
+                              color: Colors.white,
+                            ),
+                          ),
+                          child: CommonWidget.commonText(text: "Youtube",color: Colors.white),
+                        ),
+                      ),
+                      const Spacer(),
+                      Container(
+                        height: 100.h,
+                        width: 120.w,
+                        alignment: Alignment.center,
+                        padding: const EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            color:  AppColor.primary1Color,
+                            borderRadius: BorderRadius.circular(12),
+                            border: Border.all(
+                              color: Colors.white,
+                            )),
+                        child: CommonWidget.commonText(text: "Coins",color: Colors.white),
+                      ),
+                    ],
+                  ),
+                   const Spacer(),
+                  Row(
+                    children: [
+                      InkWell(
+                        onTap: () => CommonRoute.pushNamed(
+                          page: RouteList.admin_user_data_screen,
+                        ),
+                        child: Container(
+                          height: 100.h,
+                          width: 120.w,
+                          alignment: Alignment.center,
+                          padding:  EdgeInsets.all(10.r),
+                          decoration: BoxDecoration(
+                            color:  AppColor.primary1Color,
+                            borderRadius: BorderRadius.circular(12),
+                            border: Border.all(
+                              color: Colors.white,
+                            ),
+                          ),
+                          child: CommonWidget.commonText(text: "Notification",color: Colors.white),
+                        ),
+                      ),
+                      const Spacer(),
+                      Container(
+                        height: 100.h,
+                        width: 120.w,
+                        alignment: Alignment.center,
+                        padding: const EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            color:  AppColor.primary1Color,
+                            borderRadius: BorderRadius.circular(12),
+                            border: Border.all(
+                              color: Colors.white,
+                            )),
+                        child: CommonWidget.commonText(text: "Category",color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ],
               ),
-            )
+            ),
+          ),
         ],
-      )
+      ),
     );
   }
 }
